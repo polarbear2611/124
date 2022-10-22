9214599dcf53befab9a24c503c0378b9360de9fe
polarbear2611 <98675499+polarbear2611@users.noreply.github.com>
2022-10-22 15:58:43 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+
+function setup()
+{
+    video =createCapture(VIDEO);
+    video.size(550 , 500);
+    canvas = createCanvas(550 , 550);
+    canvas.position(560, 150);
+    posenet  = ml5.poseNet(video, modelLoaded);
+    posenet.on("pose", gotPoses);
+}
+
+function draw()
+{
+    background("white");
+}
+
+
+function modelLoaded()
+{
+    console.log("poseNet is initialized!")
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
